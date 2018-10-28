f49b450cba565e3f7457987fff2f48f4dd242ef3
PolinaMoiseyenko <pmoiseyenko@gmail.com>
2018-10-28 20:33:56 +0300

    feat: add recursion and tests for it

--- tests.js
+++ tests.js
@@ -57,3 +57,10 @@ describe('make', function () {
       });
 
 });
+
+const recursion = require('./src/recursion');
+describe('recursion', function () {
+    it('Test', () => {
+        assert.deepEqual(recursion({"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}}), [ [ 100 ], [ 90, 120 ], [ 70, 99, 110, 130 ] ]);
+    });
+});
